ff00fbd3bcfb2a0363b22214f9b351e02afab90e
Filip Bontin <fbontin@gmail.com>
2017-05-17 21:45:46 +0200

    WIP: fetching all data without parsing

--- src/app/politician-fetcher.service.ts
+++ src/app/politician-fetcher.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { POLITICIAN } from './mock-politician';
+import { Http } from '@angular/http';
+import 'rxjs/Rx'
 
 @Injectable()
 export class PoliticianFetcherService {
 
-  constructor() { }
+  constructor(private http: Http) { }
   //Returns a specific politicians based on their intressent_id.
   getPolitician(){
     //Currently return mock data from the file './mock-politician'
@@ -13,7 +15,15 @@ export class PoliticianFetcherService {
   }
   //Returns all currently active politicians.
   getAllPoliticians(){
+      const url = "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=";
+      var allPoliticians = [];
 
+
+      this.http.get(url)
+        .map(res => res.json())
+        .subscribe(result => allPoliticians = result);
+
+      console.log(allPoliticians);
   }
 
 }
