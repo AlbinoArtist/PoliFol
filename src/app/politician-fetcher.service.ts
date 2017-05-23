d7cf040e6c5ceaa5af90721f9cbabd8d51a59967
Filip Bontin <filip.bontin@valtech.se>
2017-05-23 23:25:40 +0200

    Make getAllPoliticians() work

--- src/app/politician-fetcher.service.ts
+++ src/app/politician-fetcher.service.ts
@@ -1,29 +1,26 @@
 import { Injectable } from '@angular/core';
 import { POLITICIAN } from './mock-politician';
 import { Http } from '@angular/http';
-import 'rxjs/Rx'
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PoliticianFetcherService {
 
   constructor(private http: Http) { }
+
   //Returns a specific politicians based on their intressent_id.
   getPolitician(){
     //Currently return mock data from the file './mock-politician'
     return POLITICIAN;
-
   }
+
   //Returns all currently active politicians.
   getAllPoliticians(){
-      const url = "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=";
-      var allPoliticians = [];
-
-
-      this.http.get(url)
-        .map(res => res.json())
-        .subscribe(result => allPoliticians = result);
-
-      console.log(allPoliticians);
+    const url = "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=";
+    
+    return this.http.get(url)
+      .map(response => response.json())
+      .map(response => response.personlista.person);
   }
 
 }
