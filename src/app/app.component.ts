58b26f3c1514a236ecb0b7d34f4c7dd93cb41720
Filip Bontin <filip.bontin@valtech.se>
2017-05-23 23:26:42 +0200

    Add getAllPoliticians() example to start page
    Should probably be removed later, but might be good to have still.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { PoliticianFetcherService } from './politician-fetcher.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [PoliticianFetcherService]
 })
 export class AppComponent {
   title = 'app works!';
   test = 'test';
+  politicians = {};
+
+  constructor(private fetcher: PoliticianFetcherService) {  }
+
+  ngOnInit() {
+    this.fetcher.getAllPoliticians()
+      .subscribe( data => this.politicians = data);
+  }
 }
