c787e4733a94ae6525a7be8db84f2d61acf84806
Filip Bontin <filip.bontin@valtech.se>
2017-05-29 19:19:00 +0200

    Generate component for politicians dropdown

---
+++ src/app/politicians-dropdown/politicians-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-politicians-dropdown',
+  templateUrl: './politicians-dropdown.component.html',
+  styleUrls: ['./politicians-dropdown.component.scss']
+})
+export class PoliticiansDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
